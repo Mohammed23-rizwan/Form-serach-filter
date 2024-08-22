e49b29f6b604daf5de2068684bd34f7b450dc35c
Mohammed23-rizwan <rizuart23@gmail.com>
2024-08-22 15:46:29 +0530

    with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import "./users";
-import { Users } from "./users";
+// import { Users } from "./users";
 import Table from "./component/table";
+import axios from "axios";
 
 function App() {
   const [query, setquery] = useState(0);
-  const search = (data) => {
-    return data.filter(
-      (item) =>
-        item.first_name.toLowerCase().includes(query) ||
-        item.last_name.toLowerCase().includes(query) ||
-        item.email.toLowerCase().includes(query)
-    );
-  };
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const res = await axios.get(`http://localhost:8800?q=${query}`);
+      setdata(res.data);
+    };
+    fetchdata();
+  }, [query]);
+  // const search = (data) => {
+  //   return data.filter(
+  //     (item) =>
+  //       item.first_name.toLowerCase().includes(query) ||
+  //       item.last_name.toLowerCase().includes(query) ||
+  //       item.email.toLowerCase().includes(query)
+  //   );
+  // };
   return (
     <>
       <div className="app">
@@ -27,7 +37,8 @@ function App() {
             setquery(e.target.value);
           }}
         />
-        <Table data={search(Users)} />
+        {/* <Table data={search(Users)} /> */}
+        <Table data={data} />
       </div>
     </>
   );
